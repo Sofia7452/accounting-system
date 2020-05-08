f4a3fb5a9dda2a7a35035806d4c38c13aeba6ca9
gaojia <gaojia@goodrock.com.cn>
2020-05-08 17:11:32 +0800

    创建views目录

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
-import Layout from './components/layout';
+import Tags from './views/Tags';
+import Statistics from './views/Statistics';
+import Money from './views/Money';
+import NoMatch from './views/NoMatch';
 
 function App() {
   return (
@@ -22,24 +25,6 @@ function App() {
       </Switch>
     </Router>
   );
-}
-
-function NoMatch() {
-  return (
-    <div>页面不存在，输错地址了吧</div>
-  );
-}
-
-function Statistics() {
-  return <Layout><h2>统计页面</h2></Layout>
-}
-
-function Tags() {
-  return <Layout><h2>标签页面</h2></Layout>
-}
-
-function Money() {
-  return <Layout><h2>记账页面</h2></Layout>
 }
 
 export default App;
---
+++ src/views/Statistics.tsx
@@ -0,0 +1,7 @@
+import Layout from '../components/layout';
+import React from 'react';
+
+function Statistics() {
+  return <Layout><h2>统计页面</h2></Layout>
+}
+export default Statistics
