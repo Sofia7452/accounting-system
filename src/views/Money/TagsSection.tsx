d0523c10f2fb39f74472c0c109b2ea9672c5a93e
gaojia <sofia7452@163.com>
2020-12-11 12:48:43 +0800

    抽离useTags函数

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import React, {useState} from 'react';
+import React from 'react';
+import {useTags} from '../../useTags';
 
 const Wapper = styled.section`
   background: #ffffff;
@@ -37,7 +38,7 @@ type Props = {
 };
 // 函数的所有类型参数都是由<>尖括号包起来的
 const TagsSection: React.FC<Props> = (props) => {
-  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const {tags, setTags} = useTags();
   const selectedTags = props.value
   const onAddTag = () => {
     const tagName = window.prompt("新标签的名称为");
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,7 +1,9 @@
 import Layout from '../components/layout';
 import React from 'react';
+import {useTags} from '../useTags';
 
 function Tags() {
+  const {tags,setTags} = useTags()
   return <Layout><h2>标签页面</h2></Layout>
 }
 export default Tags
