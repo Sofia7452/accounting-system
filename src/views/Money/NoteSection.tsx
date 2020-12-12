5acbbb1a6005cbf95d6f71e36e8a781eb46fa26e
gaojia <sofia7452@163.com>
2020-12-12 22:33:56 +0800

    创建单独的Input组件

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,51 +1,36 @@
 //⚠️： white-space: nowrap可以让内容不换行
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import {Input} from '../../components/input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-  >label{
-    display: flex;
-    align-items: center;
-    >span{margin-right: 16px; white-space: nowrap}
-    >input{
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
+  
 `;
 type Props = {
-  value:string,
-  onChange:(value:string)=>void
+  value: string,
+  onChange: (value: string) => void
 }
 const NoteSection: React.FC<Props> = (props) => {
-  const note = props.value
+  const note = props.value;
   const refInput = useRef<HTMLInputElement>(null);
-  const onBlur = () => {
-    if (refInput.current) {
-      console.log(refInput.current.value);
-      props.onChange(refInput.current.value);
-    }
+  const onChange:ChangeEventHandler<HTMLInputElement> = (e) => {
+    props.onChange(e.target.value);
   };
   return (
     <Wrapper>
-      <label>
-        <span>备注</span>
-        <input type='text' placeholder='在这里添加备注'
-          // 1.受控：过程变化都控制
-          // value={note} onChange={e=>props.onChange(e.target.value)}
-          // 2.非受控：过程变化不关心，关心最后的结果
-          // 这里必须用defaultValue
-               ref={refInput} defaultValue={note} onBlur={onBlur}
-          // 3.细节注意：React onChange会在输入一个字的时候就触发
-          //  HTML onchange 在鼠标移走的时候触发，早于onBlur
-        />
-      </label>
+      <Input label='备注' placeholder='在这里添加备注'
+             value={note} onChange={onChange}
+      />
+      {/*// 1.受控：过程变化都控制*/}
+      {/*// value={note} onChange={e => props.onChange(e.target.value)}*/}
+      {/*// 2.非受控：过程变化不关心，关心最后的结果*/}
+      {/*// 这里必须用defaultValue*/}
+      {/*// ref={refInput} defaultValue={note} onBlur={onBlur}*/}
+      {/*// 3.细节注意：React onChange会在输入一个字的时候就触发*/}
+      {/*// HTML onchange 在鼠标移走的时候触发，早于onBlur*/}
     </Wrapper>
   );
 };
