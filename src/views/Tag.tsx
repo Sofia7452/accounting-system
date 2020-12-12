5c4e019a55c4d357b346036c5c6195442fc3636d
gaojia <sofia7452@163.com>
2020-12-12 19:04:28 +0800

    封装findTag 到 useTags

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {useTags} from '../useTags';
+import {useParams} from 'react-router-dom';
 
 const Tag: React.FC = () => {
+  const {findTag} = useTags();
+  let {id} = useParams();
+  const tag = findTag(parseInt(id));
   return (
-    <div>hi</div>
+    <div>{tag.name}</div>
   );
 };
 export {Tag};
